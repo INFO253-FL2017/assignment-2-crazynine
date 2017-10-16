890fb674a6ad7972debf99038511e61868090548
crazynine <crazynine@berkeley.edu>
2017-10-16 03:33:24 +0000

    almost done

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
 // script.js
+//var contactForm = document.getElementById("contactForm");
+//contactForm.addEventListener("submit", function(event) {
+//	var name = contactForm.elements.namedItem("name").value;
+//	var subject = contactForm.elements.namedItem("subject").value;
+//	var message = contactForm.elements.namedItem("message").value;
+//	var alert = document.getElementById("alert");
+//	if (name === "") {
+//		alert.innerHTML = "Please fill out the Name box!"
+//	} else if (subject === "") {
+//		alert.innerHTML = "please fill out the Subject box!"
+//	} else if (message === "") {
+//		alert.innerHTML = "Please fill out the Message box!"
+//	} else {
+//		alert.innerHTML = "Hi " + name +  ", your message has been sent"
+//	}
 
-console.log("js/script.js has been loaded!");
+//	event.preventDefault();
+//})
--- webserver.py
+++ webserver.py
@@ -3,17 +3,80 @@ webserver.py
 
 File that is the central location of code for your webserver.
 """
-
-from flask import Flask, render_template
+import requests
+import os
+from flask import Flask, request, render_template
 
 # Create application, and point static path (where static resources like images, css, and js files are stored) to the
 # "static folder"
 app = Flask(__name__,static_url_path="/static")
 
 @app.route('/')
-def hello_world():
+def home():
     """
     If someone goes to the root of your website (i.e. http://localhost:5000/), run this function. The string that this
     returns will be sent to the browser
     """
     return render_template("index.html") # Render the template located in "templates/index.html"
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
+
+@app.route('/m1')
+def m1():
+	return render_template("m1.html")
+
+
+@app.route('/m2')
+def m2():
+	return render_template("m2.html")
+
+@app.route('/m3')
+def m3():
+	return render_template("m3.html")
+
+@app.route('/m4')
+def m4():
+	return render_template("m4.html")
+
+@app.route('/m5')
+def m5():
+	return render_template("m5.html")
+
+@app.route('/contact', methods=['POST'])
+def send_email():
+	notifications = []
+	name = request.form.get("name")
+	subject = request.form.get("subject")
+	message = request.form.get("message")
+
+	if (name == ""):
+		notifications.append("Please fill out the name box below")
+	elif(subject == ""):
+		notifications.append("Please fill out the subejct box below")
+	elif(message == ""):
+		notifications.append("Please fill out the message box below")
+	else:
+		data = {
+	    'from': os.environ["INFO253_MAILGUN_FROM_EMAIL"],
+		'to': os.environ["INFO253_MAILGUN_TO_EMAIL"],
+		'subject': subject,
+		'text': message,
+		}
+		auth = (os.environ["INFO253_MAILGUN_USER"], os.environ["INFO253_MAILGUN_PASSWORD"])
+		r = requests.post(
+			'https://api.mailgun.net/v3/{}/messages'.format(os.environ["INFO253_MAILGUN_DOMAIN"]),
+			auth=auth,
+			data=data)
+
+		if (r.status_code == requests.codes.ok):
+			notifications.append("Hi " + name + ", your message was sent")
+		else:
+			notifications.append("You email was not sent. Please try again later")
+
+	return render_template("contact.html", notifications=notifications)
